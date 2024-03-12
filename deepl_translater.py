c15ce9b13bfa811aa3e54655de363551934021fd
Kid-Chang <pch3310@gmail.com>
2024-03-12 19:59:28 +0900

    fix: 빈 값을 읽어 오류가 발생하는 현상 수정

--- deepl_translater.py
+++ deepl_translater.py
@@ -10,6 +10,9 @@ translator = deepl.Translator(auth_key)
 
 
 def translate_press_content_line(original_content_line):
+    if len(original_content_line) == 0:
+        print("빈 라인입니다.")
+        return ""
     # deepL API를 이용한 번역
     result = translator.translate_text(original_content_line, target_lang="KO")
 
