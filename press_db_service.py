ec088ea1ace752ecb72a8b09e0b324fbbc297c31
Kid-Chang <pch3310@gmail.com>
2024-04-28 23:23:40 +0900

    fix: 문단 단위로 정하는 문제를, 문단 끝에 구분자(!@!)를 두고, 모든 문장 끝(.)을 기준으로 분리해서 문장으로서 저장하는 것으로 처리. 프론트에서 문단 구분자에 대한 처리가 필요.

--- press_db_service.py
+++ press_db_service.py
@@ -1,3 +1,5 @@
+import re
+
 from crud import CRUD
 from deepl_translater import translate_press_content_line
 
@@ -17,14 +19,27 @@ class PressDbService(CRUD):
 
         # 뉴스 텍스트 리스트로 분리
         content_list = content.split('\n')
+        # 각 라인의 길이가 0이거나 '﻿'인 경우 제외
         filtered_content_list = [c for c in content_list if len(c) > 0 and c != '﻿']
+        # 각 라인 끝에 구분자 추가
+        content_list = [c + '!@!' for c in filtered_content_list]
+        # 각 라인을 ". " 단위 리스트로 분리
+        separated_content_list = [re.split(r'(?<=\.)\s+', c) for c in content_list]
+        # 이중 리스트를 다시 1차원 리스트로 변환
+        separated_content_list = [item for sublist in separated_content_list for item in sublist]
 
         # 뉴스 텍스트 개별 번역 및 저장
-        for line_number, content in enumerate(filtered_content_list):
+        for line_number, content in enumerate(separated_content_list):
             # 나중에 벌크 연산 이용해보면 좋을듯
             line_number += 1
             print("press_id: ", last_press_id, "line_number: ", line_number, "content: ", content)
+            if content.__contains__('!@!'):
+                removed = content.replace('!@!', '')
+                translated_content = translate_press_content_line(removed)
+                self.insertPressContentDB(last_press_id, line_number, content, translated_content)
+                continue
             translated_content = translate_press_content_line(content)
+
             self.insertPressContentDB(last_press_id, line_number, content, translated_content)
 
         print(original_url, "업로드 완료")
