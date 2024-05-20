f9ee9c6f887edba21438ff76e312f351551f3606
Kid-Chang <pch3310@gmail.com>
2024-05-20 21:35:11 +0900

    feat: translate_press_content_line 함수의 타겟 언어 지정 가능

--- deepl_translater.py
+++ deepl_translater.py
@@ -9,12 +9,12 @@ auth_key = os.environ.get('DEEPL_AUTH_KEY')
 translator = deepl.Translator(auth_key)
 
 
-def translate_press_content_line(original_content_line):
+def translate_press_content_line(original_content_line, target_lang="KO"):
     if len(original_content_line) == 0:
         print("빈 라인입니다.")
         return ""
     # deepL API를 이용한 번역
-    result = translator.translate_text(original_content_line, target_lang="KO")
+    result = translator.translate_text(original_content_line, target_lang=target_lang)
 
     print(result.text)
     return result.text
