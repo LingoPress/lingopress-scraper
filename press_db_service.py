6fc8b5602c65f3188ac0df02f4e3b7b0646141c0
Kid-Chang <pch3310@gmail.com>
2024-06-13 19:55:24 +0900

    feat: 여러 언어를 지원함에 따라 번역하는 언어를 인식하도록 변경

--- press_db_service.py
+++ press_db_service.py
@@ -1,6 +1,5 @@
 import re
 import spacy
-
 from crud import CRUD
 from deepl_translater import translate_press_content_line
 
@@ -15,8 +14,6 @@ class PressDbService(CRUD):
         if self.check_exist_url(original_url):
             print(original_url, "이미 존재하는 기사입니다.")
             return
-
-
 
         nlp = spacy.load(self.nlp_model)
         doc = nlp(content)
@@ -33,16 +30,26 @@ class PressDbService(CRUD):
 
         print("press_id: ", last_press_id)
 
-        translated_title_ko = translate_press_content_line(title)
-        translated_title_ja = translate_press_content_line(title, "ja")
+        # TODO: 원문 언어를 제외한 언어로 제목 번역
+        print("@@@@@ lang", language)
+
+        # en is deprecated. use en-us
+        support_language_list = ["ko", "ja", "en-us"]
+        support_language_list.remove(language)
+        for target_lang in support_language_list:
+            translated_title = translate_press_content_line(title, target_lang)
+            self.insertDB("press_translation", "press_id, translated_title, translated_language",
+                          (last_press_id, translated_title, target_lang))
+        # translated_title_ko = translate_press_content_line(title)
+        # translated_title_ja = translate_press_content_line(title, "ja")
 
         # 한국어 제목
-        self.insertDB("press_translation", "press_id, translated_title, translated_language",
-                      (last_press_id, translated_title_ko, "ko"))
+        # self.insertDB("press_translation", "press_id, translated_title, translated_language",
+        #              (last_press_id, translated_title_ko, "ko"))
 
         # 일본어 제목
-        self.insertDB("press_translation", "press_id, translated_title, translated_language",
-                      (last_press_id, translated_title_ja, "ja"))
+        # self.insertDB("press_translation", "press_id, translated_title, translated_language",
+        #              (last_press_id, translated_title_ja, "ja"))
 
         # 뉴스 텍스트 개별 번역 및 저장
         for line_number, content in enumerate(sentences):
