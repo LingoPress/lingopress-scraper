366aafdc59ccf2ba4546bdaf38baf0cc2a3e6960
Kid-Chang <pch3310@gmail.com>
2024-06-13 19:54:36 +0900

    fix: deepl때문에 en->en-us로 수정

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from rss_based_news_scraper import rss_based_news_scraper
 
 
 rss_link_list = [
-    {"lang": "en", "rss": "https://globalvoices.org/feed", "nlp_model": "en_core_web_sm"},
+    {"lang": "en-us", "rss": "https://globalvoices.org/feed", "nlp_model": "en_core_web_sm"},
     {"lang": "ja", "rss": "https://jp.globalvoices.org/feed", "nlp_model": "ja_core_news_md"}
 ]
 
@@ -11,5 +11,3 @@ for rss in rss_link_list:
     print("@@@: ", rss['lang'], "뉴스 저장 시작 --")
     rss_based_news_scraper(rss['lang'], rss['rss'], rss['nlp_model'])
     print("@@@: ", rss['lang'], "뉴스 저장 완료 --")
-
-print("일본어 뉴스 저장")
