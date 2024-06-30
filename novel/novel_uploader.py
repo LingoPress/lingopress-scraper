76ca236e27e699107e66b66ebacbcb4e740a6938
Kid-Chang <pch3310@gmail.com>
2024-06-30 14:36:38 +0900

    feat: 저장된 csv 파일을 읽어 업로드 하는 함수

---
+++ novel/novel_uploader.py
@@ -0,0 +1,30 @@
+# csv 파일 db에 올리기
+import os
+import sys
+import novel_csv_reader
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from press_db_service import PressDbService
+
+
+def novel_upload(file_path, title, authors, language):
+    content = novel_csv_reader.read_csv_file(file_path)
+    press = {
+        'title': title,
+        'content': content,
+        'url': None,
+        'published_at': None,
+        'image_url': "",
+        'authors': authors,
+        'language': language,
+        "publisher": "",
+        'access_level': 'public',
+        'category': 'NOVEL'
+    }
+    press_db_service = PressDbService(None)
+    press_db_service.uploadPressDB(press['title'], press['content'], press['url'], press['published_at'],
+                                   press['image_url'], press['authors'], press['language'], press['publisher'],
+                                   press['access_level'], press['category'])
+
+
+novel_upload("../novels/물고기비늘옷_ja.csv", "魚服記", "太宰治", "ja")
